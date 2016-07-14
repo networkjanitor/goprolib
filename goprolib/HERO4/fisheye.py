9f68bcd5ce09dd5c29b1a7ffd3f1e55c5ae3d80d
Tinywave Netsphere <xyoc@hotmail.de>
2016-07-14 23:09:12 +0200

    cleanup fisheye enum

--- goprolib/HERO4/fisheye.py
+++ goprolib/HERO4/fisheye.py
@@ -1,12 +1,5 @@
 import aenum
 
-#4 x 3 W 94.4	122.6	149.2
-#4 x 3 M	72.2	94.4	115.7
-#4 x 3 N	49.1	64.6	79.7
-#16 x 9 W	69.5	118.2	133.6
-#16 x 9 M	55	94.4	107.1
-#16 x 9 N	37.2	64.4	73.6
-
 # Fisheye
 class Fisheye(aenum.Enum):
     WIDE_4x3 = [0.0944, -0.1226, -0.1492]
@@ -19,5 +12,5 @@ class Fisheye(aenum.Enum):
 
 
 if __name__ == '__main__':
-    x= Fisheye.WIDE_4x3
+    x = Fisheye.WIDE_4x3
     print(x.value)
